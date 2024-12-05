8046fe3dc7edb3f572047f60582b795dc27399cf
Fuya IWASHIMA <c1102668@planet.kanazawa-it.ac.jp>
2024-12-05 18:50:11 +0900

    ニュース取得

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -92,13 +92,7 @@ function App() {
             </div>
             <div className="slack-containar">
             {news.map((article, index) => (
-              <li key={index} style={{ marginBottom: "20px", borderBottom: "1px solid #ccc", paddingBottom: "10px" }}>
-                <h2 style={{ fontSize: "1.2rem", color: "#333" }}>{article.title}</h2>
                 <p>{article.description}</p>
-                <a href={article.url} target="_blank" rel="noopener noreferrer" style={{ color: "blue" }}>
-                  記事を読む
-                </a>
-              </li>
             ))}
             </div>
                 <div className="bus-containar">
